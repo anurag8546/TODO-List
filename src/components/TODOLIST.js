9ad1b2522d6814c80e9d2d4a5938b2389665dc92
Anurag <2000520100019@ietlucknow.ac.in>
2022-06-16 03:00:22 +0530

    added new file and updated TODOLIST.js

--- src/components/TODOLIST.js
+++ src/components/TODOLIST.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import TODOFORM from './TODOFORM';
 function TODOLIST(){
 
-    const {todos, setTodos}= useState([]);
+    const [todos, setTodos]= useState([]);
 
 //It'll add todos in list
 const addTodos=todo_input=>{
@@ -11,18 +11,58 @@ const addTodos=todo_input=>{
     if(!todo_input.text || /^\s*$/.test(todo_input.text)){
         return;
     }
-    const newTodos={todo_input, ...todos};
+    const newTodos=[todo_input, ...todos];
 
     setTodos(newTodos);
+    //it'll print the inputs in console along with it's id
     console.log(todo_input, ...todos);
 };
+
+
+// updating
+
+const updateTODO=(todoId,newValue)=>{
+    if(!newValue.text || /^\s*$/.test(newValue.text)){
+        return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+ 
+};
+
+
+
+//Delete an Item from todo list
+
+const removeTODO=id=>{
+    const removedArray=[...todos].filter(todo_input=>todo_input.id!==id);
+    setTodos(removedArray);
+};
+
+
+const completeTodo = id => {
+    let updatedTodos=todos.map(todo => {
+      if (todo.id===id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+
+
   return(
-    <div>
+<>
+<div>
         <h1>
             Whats up?
         </h1>
     <TODOFORM onSubmit={addTodos}/>
+    
+    
     </div>
+    </>
     );
 }
 
